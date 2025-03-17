fc9229b23d5e3ee589785a4d1d328a8ebef3cef9
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-03-17 11:21:46 +0700

    perulangan do while

--- LoopingSwitchCase_0033/contoh_looping.cpp
+++ LoopingSwitchCase_0033/contoh_looping.cpp
@@ -27,15 +27,38 @@ int main()
     cout << endl;
     
     cout << "perulangan while" << endl;
+
     srand(time(0));
+
     n = rand() % 10;
+
     cout << "nilai awal n = " << n << endl;
+
     while (n < 7)
     {
         cout << "nilai n = " << n << endl;
-        n = rand() % 10
+        n = rand() % 10;
         perulanganWhile++;
     }
+
     cout << "jumlah perulangan while = " << perulanganWhile << endl;
     cout << "nilai n terakhir = " << n << endl;
+
+    cout << "perulangan do while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10
+
+    cout << "nilai awal n = " << n << endl;
+
+    do
+    {
+        cout << "nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganDo++;
+    } while (n < 7);
+    cout << "jumlah perulangan = " << perulanganWhile << endl;
+    cout << "jumlah perulangan = " << n << endl;
+    
 }
