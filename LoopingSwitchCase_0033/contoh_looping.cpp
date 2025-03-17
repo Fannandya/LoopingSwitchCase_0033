61de7d03605b161c9ac5bbda9e370268e4033ba4
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-03-17 10:56:20 +0700

    perulangan for pencacah naik

--- LoopingSwitchCase_0033/contoh_looping.cpp
+++ LoopingSwitchCase_0033/contoh_looping.cpp
@@ -8,4 +8,12 @@ int main()
     int perulanganDo = 0;
 
     // perulangan for
+    
+    cout << "perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "nilai n = " << n << "selamat datang" << endl;
+    }
+    cout << "nilai n terakhir = " << n << endl;
+    cout << endl;
 }
